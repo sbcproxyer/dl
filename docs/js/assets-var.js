44105b13b2d1bffdc43a825668550c4c829ca4b7
nbsdev <dev@nbs.com>
2021-10-08 09:12:26 +0800

    update

--- docs/js/assets-var.js
+++ docs/js/assets-var.js
@@ -1,1 +1,4 @@
+// apk 下载地址
 var ninjiaapk="https://dl.testfairy.com/download/74VK0D1G6WS2TC1DA1NS6DA2008KCKS3RAYRS87H1W6YYSPM/ninja_2.2.0-testfairy.apk"
+// 二维码内容
+var qrcontent = "https://sbcproxyer.github.io/dl/"
--- docs/js/utils.js
+++ docs/js/utils.js
@@ -7,3 +7,31 @@ function isWx(){
    　　 return false;      
 　　}
 }
+
+// https://sbcproxyer.github.io/dl/
+
+function drawQrcode(cb){
+    var text = window.qrcontent || 'https://sbcproxyer.github.io/dl/';
+    if(!QrCodeWithLogo){
+       console.error('no QrCodeWithLogo.')
+       return;
+    }
+
+
+     new QrCodeWithLogo({
+       canvas:document.querySelector('#qrCode'),
+       content:text,
+       width:220,
+       logo:{
+          src:'./assets/images/logo.png',
+          logoSize:0.25,
+          borderSize:'2px',
+          borderRadius:30
+       }
+    }).toCanvas().then(()=>{
+       if(typeof cb === 'function'){
+          cb('success')
+       }
+    })
+    
+}
