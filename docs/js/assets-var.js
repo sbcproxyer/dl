5ba716b06ed266bef30a1f858cfeb8f059167aa1
alen-x <66928150+alen-x@users.noreply.github.com>
2021-10-12 20:38:20 +0800

    Update assets-var.js
    update ninjiaapk

--- docs/js/assets-var.js
+++ docs/js/assets-var.js
@@ -1,4 +1,4 @@
 // apk 下载地址
-var ninjiaapk="https://dl.testfairy.com/download/74VK0D1G6WS2TC1DA1NS6DA2008KCKS3RAYRS87H1W6YYSPM/ninja_2.2.0-testfairy.apk"
+var ninjiaapk="https://dl.testfairy.com/download/74VK2D1G6WT2TC1DHRBCFBWCG24S7R9JX4G7ME44KVBGRG0F/ninja_2.3.0-testfairy.apk"
 // 二维码内容
 var qrcontent = "https://sbcproxyer.github.io/dl/"
