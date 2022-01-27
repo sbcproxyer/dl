771b922152cc69345f38b4cb78a6023defd1305f
nbsdev <dev@nbs.com>
2022-01-27 16:59:51 +0800

    feat: 添加新下载页面

---
+++ docs/js/app-conf.md
@@ -0,0 +1,12 @@
+# app.config.js 说明
+
+
+## chatv3
+
+```js
+    chatv3:{ // 对应 chat.html 里二维码
+        title:'',
+        apkurl:'',
+        iosurl:''
+    }
+```
--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -1,9 +1,12 @@
+/**
+ * chatv3 里为新增
+ */
 window.njconfig={
     apkurl:"https://dl.testfairy.com/download/74VKGD9Q64S2TC1DDENQ7GBA86ZZ6PPEY5K8W20ZTQPZ94FV/ninja_3.0.0-testfairy.apk",
     isourl:"https://testflight.apple.com/join/2EccEH6p",
     qrdltext:"https://sbcproxyer.github.io/dl/",
     chatv3:{
-        title:'O欢迎使用加密聊天V3',
+        title:'欢迎使用加密聊天V3',
         apkurl:'https://dl.testfairy.com/download/74VKGD9Q64S2TC1DDENQ7GBA86ZZ6PPEY5K8W20ZTQPZ94FV/ninja_3.0.0-testfairy.apk',
         iosurl:'https://testflight.apple.com/join/2EccEH6p'
     }
