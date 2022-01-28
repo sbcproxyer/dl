04f5b2813f5e92456eb51066366e0ba73330d385
alen-x <66928150+alen-x@users.noreply.github.com>
2022-01-28 12:21:00 +0800

    Update app.config.js
    update android apkurl

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -7,7 +7,7 @@ window.njconfig={
     qrdltext:"https://sbcproxyer.github.io/dl/",
     chatv3:{
         title:'欢迎使用加密聊天V3',
-        apkurl:'https://dl.testfairy.com/download/74VKGD9Q64S2TC1DDENQ7GBA86ZZ6PPEY5K8W20ZTQPZ94FV/ninja_3.0.0-testfairy.apk',
+        apkurl:'https://dl.testfairy.com/download/74W34E1G68V2TC1DDTDR95R34Y2AQPVZKMVG5SGA3PYJTJD8/ninja_3.1.4-testfairy.apk',
         iosurl:'https://testflight.apple.com/join/2EccEH6p'
     }
 };
