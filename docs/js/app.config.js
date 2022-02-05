f1e6eca3fa930c90d0bf50c895f882988392124f
nbsdev <dev@nbs.com>
2022-02-05 11:48:09 +0800

    update : url

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -7,7 +7,7 @@ window.njconfig={
     qrdltext:"https://sbcproxyer.github.io/dl/",
     qrbase:"https://sbcproxyer.github.io/dl/",
     chatv3:{
-        qrpathname:'app.html',
+        qrpathname:'https://sbcproxyer.github.io/index',
         title:'欢迎使用加密聊天V3',
         apkurl:'https://dl.testfairy.com/download/74W34E1G68V2TC1DDTDR95R34Y2AQPVZKMVG5SGA3PYJTJD8/ninja_3.1.4-testfairy.apk',
         iosurl:'https://testflight.apple.com/join/2EccEH6p'
