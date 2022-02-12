68dda8aff4e3e92534cddd0e47fff3ac790a866c
alen-x <66928150+alen-x@users.noreply.github.com>
2022-02-12 22:04:50 +0800

    Update app.config.js
    update apkurl

--- docs/js/app.config.js
+++ docs/js/app.config.js
@@ -2,14 +2,14 @@
  * chatv3 里为新增
  */
 window.njconfig={
-    apkurl:"https://dl.testfairy.com/download/74W34E9N6CSJTC1DHTT2CNGZJQ0A5MS3P2P05N2A09GP4P14/ninja_3.1.6-testfairy.apk",
+    apkurl:"https://dl.testfairy.com/download/74W38E1P6WTJTC1DBB63E2VP1VM7H8E43RZYXYF0Y9CGQ7K0/ninja_3.1.7-testfairy.apk",
     isourl:"https://testflight.apple.com/join/2EccEH6p",
     qrdltext:"https://sbcproxyer.github.io/dl/",
     qrbase:"https://sbcproxyer.github.io/dl/",
     chatv3:{
         qrpathname:'https://sbcproxyer.github.io/index',
         title:'欢迎使用加密聊天V3',
-        apkurl:'https://dl.testfairy.com/download/74W34E9N6CSJTC1DHTT2CNGZJQ0A5MS3P2P05N2A09GP4P14/ninja_3.1.6-testfairy.apk',
+        apkurl:'https://dl.testfairy.com/download/74W38E1P6WTJTC1DBB63E2VP1VM7H8E43RZYXYF0Y9CGQ7K0/ninja_3.1.7-testfairy.apk',
         iosurl:'https://testflight.apple.com/join/2EccEH6p'
     }
 };
